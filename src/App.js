1ffc80765f19c39ff0f365598bfc5e0726faa3c8
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-01-13 18:38:53 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <div className="App">
+    <h1>MERN Notes</h1>
+  </div>
+  )
 }
 
 export default App;
